72165dd73bda3f6d9ae48bf45acb49c5afe3aa33
switchit <zakaria.itispin@gmail.com>
2022-10-30 01:42:29 +0100

    first commit

---
+++ microservice-produits/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://i.ibb.co/8B8s5Bb/10.jpg', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://i.ibb.co/8KfYgF4/9.jpg', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://i.ibb.co/stmdT9d/8.jpg', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://i.ibb.co/PgCjRdN/7.jpg', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://i.ibb.co/2jL9TH3/6.jpg', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://i.ibb.co/93905N1/5.jpg', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://i.ibb.co/wR3Wf5B/4.jpg', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://i.ibb.co/N7PpmvC/3.jpg', 730.0);
---
+++ microservice-produits/src/test/java/com/mproduits/MproduitsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mproduits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MproduitsApplicationTests.class)
+public class MproduitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
