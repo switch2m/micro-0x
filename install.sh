48727ca3e10eda33cb3863f1ba13f494f25acf44
switchit <72983573+switch2m@users.noreply.github.com>
2023-03-04 10:18:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt -y update
 sudo apt -y install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt -y install ansible
 sudo apt -y install python3-argcomplete
